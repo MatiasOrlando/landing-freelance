87e3b2a88bbc3f0784cefca238243a093e15deef
MatiasOrlando <matiassorlando@gmail.com>
2022-10-18 19:58:48 +0200

    componente servicios fixed and updated

--- src/components/services/ImageCard.jsx
+++ src/components/services/ImageCard.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const ImageCard = ({ img }) => {
   return (
     <div className={`w-screen lg:w-[50%] h-max`}>
-      <img src={img} className='h-auto w-full object-cover object-center' />
+      <img src={img} className="h-auto w-full object-cover object-center" />
     </div>
   );
 };
