b5e728b65fc83ba0e9fb5dd65da289f4c46e9a08
MatiasOrlando <matiassorlando@gmail.com>
2022-10-19 13:02:54 +0200

    final changes update resize & images component servicios

--- src/components/services/SectionCont.jsx
+++ src/components/services/SectionCont.jsx
@@ -5,7 +5,7 @@ import ImageCard from "./ImageCard";
 const SectionCont = ({ img, title, text, icon, reverse }) => {
   return (
     <div
-      className={`flex xl:flex-row lg:flex-row md:flex-row sm:flex-row  w-screen
+      className={`flex xl:flex-row lg:flex-row md:flex-row sm:flex-row  w-screen object-cover bg-no-repeat
       ${reverse ? "md:flex-col-reverse" : "md:flex-col"}  
       ${reverse ? "sm:flex-col-reverse" : "sm:flex-col"}  
       ${reverse ? "xs:flex-col-reverse" : "xs:flex-col"}`}
