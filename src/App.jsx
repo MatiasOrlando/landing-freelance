f554ef3818795d0957e8e522130142c04d390b1d
Rodrigo Spano <rorrospano17@gmail.com>
2022-10-24 17:14:02 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Aboutme from './components/Aboutme/Aboutme';
 import Appointment from './components/appointment/Appointment';
 import Banner from './components/banner/Banner';
 import Carrousel from './components/Carrousel/Carrousel';
-import ContactCont from './components/contact/contactCont';
+import ContactCont from './components/contact/ContactCont';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import SenContainer from './components/sentence/SenContainer';
