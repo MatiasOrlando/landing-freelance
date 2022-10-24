370e71cdf55d907932e359252872680c242726bc
MatiasOrlando <matiassorlando@gmail.com>
2022-10-24 16:27:34 +0200

    footer component styles & responsive finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,19 +20,25 @@ const Footer = () => {
           </div>
         </div>
         <div className="contactInfo flex-col  flex justify-between pt-[60px]  h-[200px] font-roboto">
-          <div className="flex w-[100px] justify-between">
-            <p className="text-[white] ">Telefono </p>
+          <div className="xl:flex flex-row lg:flex flex-row md:flex flex-row sm:flex flex-row xs:flex flex-row-reverse  w-[250px]  xl:justify-end lg:justify-end md:justify-end sm:justify-end sm:justify-center xs:justify-center">
+            <p className="text-[white] px-4  xl:text-[13px] lg:text-[13px]  md:text-[13px]  sm:text-[10px] xs:text-[7.28px] xl:pt-0 lg:pt-0 md:pt-0 sm:pt-0 xs:pt-1">
+              (357) 145 8344
+            </p>
             <img
               src="assets/whitephone.png"
-              className="w-[19.5px] h-[19.5px]"
+              className="w-[19.5px] h-[19.5px] xl:mb-0 lg:mb-0 md:mb-0 sm:mb-0 xs:mb-8"
             />
           </div>
-          <div className="flex w-[100px] justify-between">
-            <p className="text-[white]">Mail</p>
+          <div className="xl:flex flex-row lg:flex flex-row md:flex flex-row sm:flex flex-row xs:flex flex-row-reverse  xl:w-[250px] lg:w-[250px] md:w-[250px] sm:w-[150px]  xs:w-[119px] xl:justify-end lg:justify-end md:justify-end sm:justify-end sm:justify-center xs:justify-center">
+            <p className="text-[white] px-4 xl:text-[13px] lg:text-[13px]  md:text-[13px]  sm:text-[10px] xs:text-[7.28px] xl:pt-0 lg:pt-0 md:pt-0 sm:pt-0 xs:pt-1">
+              ammenichetti@gmail.com
+            </p>
             <img src="assets/whiteletter.png" className="w-[20px] h-[16px]" />
           </div>
-          <div className="flex w-[100px] justify-between">
-            <p className="text-[white]"> Dirección</p>
+          <div className="xl:flex flex-row lg:flex flex-row md:flex flex-row sm:flex flex-row xs:flex flex-row-reverse  w-[250px] xl:justify-end lg:justify-end md:justify-end sm:justify-end sm:justify-center  xs:justify-center">
+            <p className="text-[white] px-4  xl:text-[13px] lg:text-[13px]  md:text-[13px]  sm:text-[10px] xs:text-[7.28px] xl:pt-0 lg:pt-0 md:pt-0 sm:pt-0.5 xs:pt-1">
+              Córdoba Capital
+            </p>
             <img src="assets/address.png" className="w-[15.6px] h-[19.5px]" />
           </div>
         </div>
