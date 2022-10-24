90601a99f3d3ac891729e81b0a865ed57277dba0
MatiasOrlando <matiassorlando@gmail.com>
2022-10-24 03:01:31 +0200

    fixed styles section about me

--- src/components/Aboutme/Aboutme.jsx
+++ src/components/Aboutme/Aboutme.jsx
@@ -4,13 +4,13 @@ const Aboutme = () => {
   return (
     <div className="xl:flex flex-row lg:flex-row md:flex-row sm:flex-row xs:flex flex-col-reverse lg:mt-[0px] md:mt-[0px] sm:mt-[0px] xs:mt-[60px] lg:w-full md:w-full sm:w-full xl:lg:h-[600px] lg:h-[500px] md:h-[400px] sm:h-[350px]  bg-gray">
       <div className="flex items-center sm:items-start justify-center sm: justify-left  lg:w-1/2 md:w-1/2 sm:w-1/2 xs:w-full lg:h-[300px] md:h-[300px] sm:h-[350px] xs:h-[300px] ">
-        <div className="lg:w-[474px] lg:h-[300px] md:w-[350px] md:h-[250px] sm:h-[50px]  sm:w-[250px] xs:w-[400px] xl:pt-6 lg:pt-6  md:pt-6 sm:pt-0 xs:pt-0">
+        <div className="lg:w-[474px] lg:h-[300px] md:w-[350px] md:h-[250px] sm:h-[50px]  sm:w-[250px] xs:w-[400px] xl:pt-6 lg:pt-6  md:pt-3 sm:pt-1 xs:pt-0">
           <h3 className="font-roboto font-[700] xl:text-[35px] lg:text-[25px] md:text-[20px]  sm:text-[20px] xs:text-[16px]  sm:pt-[8px] text-secondary">
             SOBRE MÍ
           </h3>
           {/* lg:w-2/5 md:w-1/2 sm:w-2/3 xs:w-[450px] lg:h-[300px] md:h-[250px]
           sm:h-[185px] */}
-          <p className="xl:text-[18px] lg:text-[14px] md:text-[10px] sm:text-[10px] xs:text-[10px] text-justify xl:leading-[28px] lg:leading-[28px] md:leading-[22px] sm:leading-[16px] xs:leading-[16px] font-roboto">
+          <p className="xl:text-[18px] lg:text-[14px] md:text-[12px] sm:text-[10px] xs:text-[10px] text-justify xl:leading-[28px] lg:leading-[28px] md:leading-[22px] sm:leading-[16px] xs:leading-[16px] font-roboto">
             Mi nombre real es Aylén Marianela Menichetti. Tengo 28 años y
             actualmente vivo en Córdoba capital. Mi aventura como Aymana Pley
             comenzó en aquellos tiempos de encierro, donde la pandemia nos tenia
