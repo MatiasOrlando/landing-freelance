7972a37c1255a30f1fe7c7333a4ce11d44e5861c
MatiasOrlando <matiassorlando@gmail.com>
2022-10-24 03:39:56 +0200

    styles in banner component fixed

--- src/components/appointment/Button.jsx
+++ src/components/appointment/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Button = ({ text, wsp = false }) => {
   return (
-    <button className="xs:mb-10 font-[500] text-[#fff] bg-secondary rounded-lg w-[162px] lg:w-[280px] h-[39px] lg:h-[64px] hover:bg-[#f9f9f9] hover:text-secondary shadow-md ease-linear transition-all duration-200">
+    <button className="mb-8 font-[500] text-[#fff] bg-secondary rounded-lg w-[162px] lg:w-[280px] h-[39px] lg:h-[64px] hover:bg-[#f9f9f9] hover:text-secondary shadow-md ease-linear transition-all duration-200">
       <a
         target="_blank"
         href={
