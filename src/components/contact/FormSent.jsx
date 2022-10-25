37b2132983538d6c6b4c6041b3047ac0f6b1b7bb
MatiasOrlando <matiassorlando@gmail.com>
2022-10-25 12:44:10 +0200

    styles added form component in responsive

--- src/components/contact/FormSent.jsx
+++ src/components/contact/FormSent.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 const FormSent = ({ setState }) => {
   return (
-    <div className="px-4 flex flex-col gap-4 font-roboto">
-      <h1 className="font-bold text-[24px]">
-        ¡Muchas gracias por tu consulta!
-      </h1>
-      <p className="w-auto">
-        Te estaremos respondiendo lo más pronto posible al correo indicado.
-      </p>
-      <button
-        onClick={() => setState(false)}
-        className=" w-[327px] lg:w-[370px] h-[64px] text-[#fff] bg-secondary rounded-[8px]"
-      >
-        Enviar otra consulta
-      </button>
+    <div className="flex flex-col gap-4 font-roboto">
+      <div className="flex-col space-y-8 text-center pt-6">
+        <h1 className="font-bold text-[24px]">
+          ¡Muchas gracias por tu consulta!
+        </h1>
+        <p className="w-auto">
+          Te estaremos respondiendo lo más pronto posible al correo indicado.
+        </p>
+        <button
+          onClick={() => setState(false)}
+          className=" w-[327px] lg:w-[370px] h-[64px] text-[#fff] bg-secondary rounded-[8px]"
+        >
+          Enviar otra consulta
+        </button>
+      </div>
     </div>
   );
 };
