823d7e475f4b8b685f80f66c8e2773deae0264f4
MatiasOrlando <matiassorlando@gmail.com>
2022-10-25 12:23:29 +0200

    padding added component formContent

--- src/components/contact/Content.jsx
+++ src/components/contact/Content.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { Icons } from './Icons';
+import React from "react";
+import { Icons } from "./Icons";
 
 export const Content = () => {
   return (
-    <div className='flex flex-col w-[325px] pt-5'>
-      <h1 className='font-bold font-roboto text-[24px] pb-2'>
+    <div className="px-4 flex flex-col w-[325px] pt-5">
+      <h1 className="font-bold font-roboto text-[24px] pb-2">
         Hacenos tu consulta por mail
       </h1>
-      <p className='font-roboto w-[325px]'>
+      <p className="font-roboto w-[325px]">
         Comentame de qué se tratan tus dificultades y me pondré en contacto con
         vos de inmediato.
       </p>
-      <div className='flex flex-col gap-10'>
-        <Icons src='phone.png' title='Telefono' data='(357) 145 8344' />
+      <div className="flex flex-col gap-10">
+        <Icons src="phone.png" title="Telefono" data="(357) 145 8344" />
         <Icons
-          src='letter.png'
-          title='Mail'
-          data='ammenichetti@gmail.com
-'
+          src="letter.png"
+          title="Mail"
+          data="ammenichetti@gmail.com
+"
         />
       </div>
     </div>
